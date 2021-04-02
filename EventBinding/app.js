032740375176a338183b21ba2cc10cd100d3f9a3
Kyle Wright <kylejamwright@gmail.com>
2021-04-01 18:33:22 -0600

    event binding

---
+++ EventBinding/app.js
@@ -0,0 +1,30 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: '',
+    };
+  },
+  methods: {
+    setName(event, lastName) {
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    },
+    resetInput() {
+      this.name = '';
+    },
+  },
+  computed: {
+    fullName() {
+      return this.name === '' ? '' : this.name + ' Wright';
+    },
+  },
+});
+
+app.mount('#events');
